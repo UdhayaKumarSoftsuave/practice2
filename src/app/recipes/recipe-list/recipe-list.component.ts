eef69577a7dde59980e3d5e742b42aee94483775
kumarudhaya <kumaudhayagit@gmail.com>
2023-01-21 17:57:12 +0530

    Recipe list componrnt

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recepie.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes : Recipe[] = [
+    new Recipe("A test" , " This is used to test" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg"),
+    new Recipe("A test" , " This is used to test" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg")
+  ];
 
 }
