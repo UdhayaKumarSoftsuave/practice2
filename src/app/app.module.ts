de6bccf8ab8831cba3a046cc688bfb57e407e846
kumarudhaya <kumaudhayagit@gmail.com>
2023-01-27 14:35:00 +0530

    Create a custom Attribute directive with different approch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { BasicDirective } from './CustomDirective/basic.directive';
+import { BetterHighlightDirective } from './CustomDirective/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { BasicDirective } from './CustomDirective/basic.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    BasicDirective
+    BasicDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
