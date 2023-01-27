e9f4137a4c0640aa6e1aa00653365417437ab83d
kumarudhaya <kumaudhayagit@gmail.com>
2023-01-27 11:53:24 +0530

    Recipe detail component changes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 
 import { Recipe } from '../../recepie.model';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input , Output } from '@angular/core';
+import { outputAst } from '@angular/compiler';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,6 +11,10 @@ import { Component, Input } from '@angular/core';
 export class RecipeItemComponent {
 
   @Input('recipe') recipe !: Recipe;
+  @Output('selected') selectedRecipeItem = new EventEmitter<Recipe>();
+  onSelectRecipeItem(){
+    this.selectedRecipeItem.emit(this.recipe);
+  }
 }
 
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recepie.model';
 
 @Component({
@@ -9,8 +9,13 @@ import { Recipe } from '../recepie.model';
 export class RecipeListComponent {
 
   recipes : Recipe[] = [
-    new Recipe("A test" , " This is used to test" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg"),
-    new Recipe("A test" , " This is used to test" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg")
+    new Recipe("A test A" , "test A" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg"),
+    new Recipe("A test B" , "test B" , "https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.jpg?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg=")
   ];
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
+  getSelectedEvent(value : Recipe){
+    this.selectedRecipe.emit(value);
+  }
 
 }
