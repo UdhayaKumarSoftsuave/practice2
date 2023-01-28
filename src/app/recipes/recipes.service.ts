e9cec2255f0861a866b01f49513121eaf719d099
PUREBOOK-S14\udhay <udhayakumar.softsuave@gmail.com>
2023-01-28 13:04:46 +0530

    Adding Ingredients in Recipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,12 +1,24 @@
 import { Injectable , EventEmitter } from "@angular/core";
+import { ingredient } from "../shared/ingredient.component";
 import { Recipe } from "./recepie.model";
 
 @Injectable()
 export class RecipeService {
 
 recipes : Recipe[] = [
-    new Recipe("A test A" , "test A" , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg"),
-    new Recipe("A test B" , "test B" , "https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.jpg?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg=")
+    new Recipe(
+        "A test A" 
+        , "test A" 
+        , "https://picturetherecipe.com/wp-content/uploads/2020/01/Mutton-Rogan-Josh-by-PictureTheRecipe-1.jpg",
+        [
+            new ingredient('orange' , 50),
+            new ingredient('tomato' , 12)
+        ]),
+    new Recipe("A test B" , "test B" , "https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.jpg?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg=",
+    [
+        new ingredient('Beans' , 9),
+        new ingredient('onion' , 33)
+    ])
     ];
 
     seletedRecepie = new EventEmitter<Recipe>();
